4d1a6b565816bf4a3cb122726c82f1d1777b392f
Josefa <jmolguin@miuandes.cl>
2022-06-23 18:16:10 -0400

    terminado

--- prueba.py
+++ prueba.py
@@ -10,11 +10,10 @@ g=Grafo(3)
 g.add_arco(1,2)
 g.add_nodo()
 g.add_arco(2,3)
-g.add_nodo()
-g.add_arco(3,4)
-g.add_arco(0,8)
+
+
 
 print(castellano(g.hay_arco(2,1)) + "hay arco entre los nodos")
-print(castellano(g.hay_camino(1,3)) + "hay arco entre los nodos")
-print(castellano(g.hay_camino(0,3)) + "hay arco entre los nodos")
-print(castellano(g.hay_camino(4,1)) + "hay arco entre los nodos")
+print(castellano(g.hay_camino(1,3)) + "hay camino entre los nodos")
+print(castellano(g.hay_camino(0,3)) + "hay camino entre los nodos")
+print(castellano(g.hay_camino(3,1)) + "hay camino entre los nodos")
